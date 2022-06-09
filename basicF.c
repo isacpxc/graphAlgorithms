b0d3d820728bc7e1995a178be8e3e2ac0e8eb376
isacpxc <isacpxc@gmail.com>
2022-06-09 18:54:49 -0300

    vertices in the first position of respectives lists

--- basicF.c
+++ basicF.c
@@ -39,41 +39,104 @@ void putVerticeName(struct vertice* v,int number){
   }
   v->name[tam+2] = '\0';
 
-  printf("%s \n", v->name);
-  printf("aqui esta o nome: %x ", v->name);
+  // printf("%s \n", v->name);
+  // printf("aqui esta o nome: %x ", v->name);
 
+}
+
+int verifyPosition(int current, int ok[], int tam){
+  int check = 0;
+  for (int i=0;i<tam;i++){
+    if (ok[i] == current) return 1;
+  }
+  return 0;
 }
 
 void create_adjList(int qtdV,int qtdE, double edgeM[][qtdE]) { // por enquanto vai ficar void
 
+  /* 
+  ESTUDO DE PONTEIROS
+
+  struct vertice** p = (struct vertice**)malloc(sizeof(struct vertice*));
+  struct vertice* q = (struct vertice*)malloc(sizeof(struct vertice));
+  struct vertice exemplo;
+
+  *q=exemplo;
+  *p = q;
+  // nesse eu digo, quando acessar o que há em p, vai cair em q, mas p guarda um endereço que não é o que o malloc de q produziu,
+  // mas funciona perfeitamene para caminhar
+  printf("endereço = %i\n", p);
+  printf("endereço = %i\n", *p);
+  printf("endereço = %i\n", &q);
+  printf("endereço = %i\n", q);
+  printf("endereço = %x\n\n", *q);
+
+  p = &q;
+  //nesse eu digo, p vai receber o endereço de q, e quando acessado, vai cair lá
+  printf("endereço = %i\n", p);
+  printf("endereço = %i\n", *p);
+  printf("endereço = %i\n", &q);
+  printf("endereço = %i\n", q); */
+
+
+
+
   lista_vertices** vetor = (lista_vertices**)malloc(qtdV*sizeof(lista_vertices*));
-  struct vertice* v = (struct vertice*)malloc(sizeof(struct vertice));
-    v->back = NULL;
-    v->next = NULL;
-    v->nameV = edgeM[0][0];
-    putVerticeName(v, v->nameV);
-    v->weight = edgeM[2][0];
 
+  // printf("vertice: %d\n", (*vetor[1])->nameV);
+  
 
-  lista_vertices* elemento = (lista_vertices*)malloc(sizeof(lista_vertices));
+  // v[0] ja esta na primeira posição, agora vamos preencher as posições restantes, iniciando uma lista para cada vértice
+  // a ideia é criar um vetor, que armazena o numero do vertice que ja foi inserido na lista
+  // ex vec = [1,-1,-1,-1,-1,-1,-1] o grafo tem 7 vertices, o primeiro é o 1
+  // aqui não vamos montar a lista ainda, apenas colocar o vertice no início da lista
+  int ok[qtdV];
+  for (int i=0;i<qtdV;i++) ok[i] = -1;
+  
+  for (int i=0,indexOk=0;i<qtdE;i++){
+    if (verifyPosition(edgeM[0][i], ok, qtdV) == 0){ // if verifyPosition return 0, this vertice don't belong yet
+      struct vertice* v_current = (struct vertice*)malloc(sizeof(struct vertice));
+      v_current->back = NULL;
+      v_current->next = NULL;
+      v_current->nameV = edgeM[0][i];
+      putVerticeName(v_current, v_current->nameV);
+      v_current->weight = edgeM[2][i];
+      lista_vertices* elemento_current = (lista_vertices*)malloc(sizeof(lista_vertices));
+      *elemento_current = v_current;
+      
+      vetor[indexOk] = elemento_current;
+      ok[indexOk] = edgeM[0][i];
+      indexOk++;
+      
+    }
+  
+  
 
-  // (**vetor) = v1; -> error segmentation
-  vetor[0] = elemento;
-  *elemento = v;
+  } //end for
+  // agora a lista esta iniciada, cada vertice inserido na primeira posição
+  // for (int i=0;i<qtdV;i++) {
+  //   printf("vertice: %s\n", (*vetor[i])->name);
+  //   printf("vertice numero: %d\n", (*vetor[i])->nameV);
+  //   printf("vertice peso: %0.2f\n\n", (*vetor[i])->weight);
+  // } verificação apenas
 
-  // dava pra fazer:
-  //vetor = &elemento;
-  //elemento = &v1;
+  
+  //            -----vetor------
+  //            |  |  |  |  |  |
+  //            [] [] [] [] [] [] 
+  //            cada posição recebe um ponteiro de ponteiro
+  //            que por sua vez aponta para o cara que aponta para o struct
 
-  printf("weight = %0.2f",(**vetor[0]).weight);
+  // dava pra fazer: ??????
+  // vetor = &elemento;
+  // elemento = &v;
+  // printf("endereço = %x\n", vetor);
+  // printf("endereço = %x\n", *vetor);
+  // printf("endereço = %x\n", &elemento);
+  // printf("endereço = %x\n", elemento);
+  // printf("\n u");
+  // printf("\n u");
 
-  // for (int x = 0; x<qtdV;x++){ //for all vertices
-  //   for (int i = 0;i<qtdE;i++){ // search on the matrix
-  //     if (x == 0){
-
-  //     }
-  //   }
-  // }
 }
 
 
