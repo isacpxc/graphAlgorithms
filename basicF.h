c3d6622755d3c73797f1a1b81d06b43f000304ef
isacpxc <isacpxc@gmail.com>
2022-06-23 01:03:32 -0300

    warning added

--- basicF.h
+++ basicF.h
@@ -1,5 +1,5 @@
 
-
+// WARNING, APENAS USE A MATRIZ PARA GERAR O GRAFO INICIAL, POIS SE FOREM REMOVIDAS/ ADICIONADAS ARESTAS, NÃO VAI DAR CERTO
 typedef struct vertice* lista_vertices;
 
 lista_vertices** create_adjList(int, int, double (*)[*]);
@@ -7,4 +7,7 @@ size_t lengthInt(int);
 void putVerticeName(struct vertice*,int);
 void Grafo(lista_vertices**, int);
 int Evertice(lista_vertices**, int, int);
-int AddAresta(lista_vertices** graph, int vi, int vj, int w, int qtdV)
+int AddAresta(lista_vertices** graph, int vi, int vj, int w, int qtdV);
+int ExisteAresta(lista_vertices** vetor, int vi, int vj,double w, int qtdV);
+int Eadj(lista_vertices** vetor, int vi, int vj, int qtdV);
+int AddAresta(lista_vertices** vetor, int vi, int vj,int w, int qtdV);
